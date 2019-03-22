83ebb13688c3d7a9c2c2657da9a76bb0025d3456
agabyte <agabyte@outlook.com>
2019-03-21 23:55:13 -0700

    Downloaded vader_lexicon to /venv/nltk_data

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/levis/Documents/agaballc/projects/mulyrica/flask/venv"
+set -gx VIRTUAL_ENV "/home/levis/Documents/agaballc/projects/mulyrica/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
